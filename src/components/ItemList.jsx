239df7a0c64b32346ec3edb09f688e1771e74b00
Müjdat Çiftçi <mujdatciftci88@gmail.com>
2024-10-12 15:59:03 +0300

    edited add task func.

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,71 +1,88 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import { PiDotsSixBold } from "react-icons/pi";
 import { SlPencil } from "react-icons/sl";
 import { IoTrashOutline } from "react-icons/io5";
 import { Reorder } from "framer-motion";
-import { useState } from "react";
-import { motion } from "framer-motion";
-const initialItems = [
-  { id: 1, name: "", status: false }
-];
+import { motion, AnimatePresence } from "framer-motion";
 
-const ItemList = () => {
-  const [items, setItems] = useState(initialItems);
+const ItemList = ({ inputText, setInputText }) => {
+  const [items, setItems] = useState([]);
 
+  useEffect(() => {
+    if (inputText) {
+      setItems((prevItems) => [
+        ...prevItems,
+        { id: prevItems.length + 1, name: inputText, status: false },
+      ]);
+    }
+  }, [inputText]);
   return (
     <div className="min-h-[100px]">
       <header className="itemlist-header">
-        <h2 className="col-start-2 col-end-10 text-3xl font-semibold">Task Name</h2>
+        <h2 className="col-start-2 col-end-10 text-3xl font-semibold">
+          Task Name
+        </h2>
         <h5 className="text-xl font-semibold mx-auto">Status</h5>
         <h5 className="text-xl font-semibold mx-auto">Edit</h5>
         <h5 className="text-xl font-semibold mx-auto">Remove</h5>
       </header>
-      <Reorder.Group
-        axis="y"
-        values={items}
-        onReorder={setItems}
-        className="w-[1000px]"
-      >
-        {items.map((item) => (
-          <Reorder.Item
-            key={item.id}
-            value={item}
-            className="itemlist-item flex items-center p-2"
-            whileDrag={{ scale: 1.02, zIndex: 1 }}
-          >
-            <PiDotsSixBold className="text-[25px] mx-auto hover:cursor-move" />
-            <p className="col-start-2 col-end-10">{item.name}</p>
-            <input
-              type="checkbox"
-              name="status"
-              checked={item.status}
-              onChange={() =>
-                setItems((prev) =>
-                  prev.map((i) =>
-                    i.id === item.id ? { ...i, status: !i.status } : i
-                  )
-                )
-              }
-              className="itemlist-checkbox mx-auto"
-            />
-            <button className="mx-auto">
-              <SlPencil className="itemlist-edit" />
-            </button>
-            <motion.button
-
-              className="mx-auto"
-              onClick={() =>
-                setItems((prev) => prev.filter((i) => i.id !== item.id))
-              }
-              initial={{opacity: 0}} 
-              animate={{opacity: 1}} 
-              exit={{opacity: 0}} 
-              transition={{duration: 0.3}}
+      
+        <Reorder.Group
+          axis="y"
+          values={items}
+          onReorder={setItems}
+          className="w-[1000px]"
+        >
+          <AnimatePresence>
+          {items.map((item) => (
+            
+            <Reorder.Item
+              key={item.id}
+              value={item}
+              className="itemlist-item flex items-center p-2"
+              whileDrag={{ scale: 1.02, zIndex: 1 }}
+              initial={{ translateY: 30 }}
+              animate={{ translateY: 0 }}
+              exit={{ translateX: 100 }}
+              transition={{ duration: 0.5 }}
             >
-              <IoTrashOutline className="itemlist-remove" />
-            </motion.button>
-          </Reorder.Item>
-        ))}
-      </Reorder.Group>
+              <PiDotsSixBold className="text-[25px] mx-auto hover:cursor-move" />
+              <p className="col-start-2 col-end-10">{item.name}</p>
+              <input
+                type="checkbox"
+                name="status"
+                checked={item.status}
+                onChange={() =>
+                  setItems((prev) =>
+                    prev.map((i) =>
+                      i.id === item.id ? { ...i, status: !i.status } : i
+                    )
+                  )
+                }
+                className="itemlist-checkbox mx-auto"
+              />
+              <button className="mx-auto">
+                <SlPencil className="itemlist-edit" />
+              </button>
+              <motion.button
+                className="mx-auto"
+                onClick={() =>
+                  setItems((prev) => prev.filter((i) => i.id !== item.id))
+                }
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.3 }}
+              >
+                <IoTrashOutline className="itemlist-remove" />
+              </motion.button>
+            </Reorder.Item>
+            
+          ))}
+          </AnimatePresence>
+        </Reorder.Group>
+      
     </div>
   );
 };
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -4,23 +4,41 @@ import { useState } from "react";
 
 const ToDoList = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  
+  const [inputText, setInputText] = useState("");
+
   const showModal = () => {
     setIsModalOpen(true);
   };
+  
+ 
+  const inputChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const addTask = (e) => {
+    e.preventDefault();
+    setIsModalOpen(false);
+  };
+
+
   return (
     <div>
       <div className="todolist-container ">
         <button className="todolist-btn-add" onClick={showModal}>Add Task</button>
-        <ItemList />
+        <ItemList inputText={inputText} setInputText={setInputText} />
         <button className="todolist-btn-remove">Remove All</button>
         {isModalOpen && 
         <Modal setIsModalOpen={setIsModalOpen}>
           <form className="flex flex-col gap-8">
             <label className="relative flex flex-col justify-center mt-3">
-              <input className="w-full px-4 rounded-md h-[65px] text-sm text-white bg-[#423A6F] focus:outline-none peer " type="text" />
+              <input className="w-full px-4 rounded-md h-[65px] text-sm text-white bg-[#423A6F] focus:outline-none peer " 
+              type="text" 
+              onBlur={inputChange}
+              />
               <span className="absolute text-gray-400 px-4 font-semibold text-sm top-5 peer-focus:top-[2px] peer-focus:font-normal cursor-text transition-all duration-200">Task Name</span>
             </label>
-            <button className="todolist-btn-add mx-auto" type="submit" >Add Task</button>
+            <button className="todolist-btn-add mx-auto" type="submit" onClick={addTask} >Add Task</button>
           </form>
         </Modal>
       }
